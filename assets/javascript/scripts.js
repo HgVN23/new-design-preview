b4d45720f0349e5a93844eb51e388c2f3b69ead8
Hg <83497023+HgVN23@users.noreply.github.com>
2022-06-23 12:05:09 +0700

    Add files via upload

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,15 +1,18 @@
-const scriptList = [
-	"ads",
+const scriptListFirst = [
 	"header",
+	"ads",
+	"goTop",
+	"footer"
+]
+
+const scriptListSecond = [
 	"theme",
+	"sidebar",
 	"filter",
 	"search",
 	// "sound",
-	"sidebar",
-	"goTop",
-	"cursor",
 	"dropdown",
-	"footer"
+	"cursor"
 ];
 
 window.onload = function() {
@@ -17,11 +20,21 @@ window.onload = function() {
 	scriptsCreate.setAttribute('id','script')
 	document.querySelector("body").insertBefore(scriptsCreate, document.querySelector("body").children[0]);
 
-	for(let script of scriptList) {
+	for(let script of scriptListFirst) {
 		var scriptCreate = document.createElement('script');
 		scriptCreate.src = `https://hgvn23.github.io/new-design-preview/assets/javascript/${script}.js`;
 		// scriptCreate.src = `../assets/javascript/${script}.js`;
 		// scriptCreate.src = `assets/javascript/${script}.js`;
 		document.getElementById("script").appendChild(scriptCreate);
 	};
+	
+	const myTimeout = setTimeout(function f() {
+		for(let script of scriptListSecond) {
+			var scriptCreate = document.createElement('script');
+			scriptCreate.src = `https://hgvn23.github.io/new-design-preview/assets/javascript/${script}.js`;
+			// scriptCreate.src = `../assets/javascript/${script}.js`;
+			// scriptCreate.src = `assets/javascript/${script}.js`;
+			document.getElementById("script").appendChild(scriptCreate);
+		};
+	}, 100);
 }
